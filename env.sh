501396049d6a7014699c9983cece2d8aeb373c1d
cloudwavetechnologies <manjunath@cloudwavetechnologies.co.in>
2025-11-01 09:34:26 +0530

    added tf files

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export TF_VAR_lambda_function_name="cloudwave-dev-lambda-001"
-export TF_VAR_region="ap-south-1"
+unset TF_VAR_region
 export TF_VAR_runtime="python3.9"
 export TF_VAR_handler="lambda_function.lambda_handler"
 #export TF_VAR_role_arn="arn:aws:iam::093326771949:role/service-role/amazon-java-code-lambda-001-role-0w2vgpaj"
@@ -10,7 +10,7 @@ export TF_VAR_s3_key="lambda/lambda_function.zip"
 export TF_VAR_memory_size=256
 export TF_VAR_timeout=60
 export TF_VAR_source_path="lambda_function.zip"
-export TF_VAR_role_arn="arn:aws:iam::093326771949:role/service-role/amazon-java-code-lambda-001-role-0w2vgpaj"
+export TF_VAR_role_arn=arn:aws:iam::093326771949:role/service-role/amazon-java-code-lambda-001-role-0w2vgpaj
 export TF_VAR_tags='{
   Environment = "prod",
   Owner       = "manjunath",
