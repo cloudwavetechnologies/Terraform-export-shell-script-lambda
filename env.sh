68fc1c8e84e32f518b1e5f03f094710e3ca44222
cloudwavetechnologies <manjunath@cloudwavetechnologies.co.in>
2025-11-01 09:09:50 +0530

    added tf files

--- env.sh
+++ env.sh
@@ -5,8 +5,8 @@ export TF_VAR_region="ap-south-1"
 export TF_VAR_runtime="python3.9"
 export TF_VAR_handler="lambda_function.lambda_handler"
 #export TF_VAR_role_arn="arn:aws:iam::093326771949:role/service-role/amazon-java-code-lambda-001-role-0w2vgpaj"
-export TF_VAR_bucket="statefile-bucket-002"
-export TF_VAR_s3_key="lambda_function.zip"
+export TF_VAR_bucket="supplychain-s3-000"
+export TF_VAR_s3_key="lambda/lambda_function.zip"
 export TF_VAR_memory_size=256
 export TF_VAR_timeout=60
 
